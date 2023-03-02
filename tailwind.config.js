90f1ddb6af434bec29c7df25fd675b6b8fa4d3ff
leandrogavidia <leandergs1234@gmail.com>
2023-03-02 10:01:40 -0400

    feat: add tailwindcss config, pages folder, styles folder

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      "white": "#ffffff",
+      "white-text": "#ffffff",
+      "dark-background": "#000000",
+      "facebook": "#2374E1",
+      "instagram": "linear-gradient(#F38334, #DA2E7D, #6B54C6)",
+      "twitter": "#55ACEE",
+      "linkedin": "#0A66C2",
+      "github": "#161B22",
+      "primary": "#FF0000",
+      "secondary": "#950101",
+      "third": "#161B22",
+      "gradient": "linear-gradient(#950101, #FF0000)"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
